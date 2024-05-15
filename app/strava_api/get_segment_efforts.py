ba5e6095a60bf99594c8bc171f98eda1bb453ed8
robgriffin247 <robgriffinwork@gmail.com>
2024-05-15 13:32:08 +0200

    added get_athlete, segment_efforts and tokens functions

---
+++ app/strava_api/get_segment_efforts.py
@@ -0,0 +1,33 @@
+import httpx
+import os
+from dotenv import load_dotenv
+import pprint as pp
+
+load_dotenv()
+
+
+def get_segment_efforts(id):
+
+    content = []
+
+    for page in range(1, 3):
+
+        response = httpx.get(f"https://www.strava.com/api/v3/segment_efforts?segment_id={id}", 
+                             headers={"Authorization":f"Bearer {os.getenv('strava_access_token')}"},
+                             params={"per_page":5, "page":page})
+    
+        response.raise_for_status
+    
+        content = content + response.json()
+
+    return content
+
+
+#data = get_segment_efforts("4458166")
+data = get_segment_efforts("17267489")
+
+
+effort_ids = [effort["start_date"] for effort in data]
+print(len(data))
+pp.pprint(effort_ids, indent=2)
+
---
+++ app/strava_api/get_tokens.py
@@ -0,0 +1,27 @@
+import httpx
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_access_token():
+    response = httpx.post("https://www.strava.com/oauth/token?",
+                          data={
+                              "client_id": os.getenv("strava_client_id"),
+                              "client_secret": os.getenv("strava_client_secret"),
+                              "code": os.getenv("strava_authorization_code"),
+                              "grant_type": "authorization_code"
+                          })
+
+    return response.json()["access_token"]
+
+def get_refresh_token():
+    response = httpx.post("https://www.strava.com/oauth/token?",
+                          data={
+                              "client_id": os.getenv('strava_client_id'),
+                              "client_secret": os.getenv('strava_client_secret'),
+                              "refresh_token": os.getenv('strava_refresh_token'),
+                              "grant_type": "refresh_token"
+                          })
+
+    return response.json()["refresh_token"]
