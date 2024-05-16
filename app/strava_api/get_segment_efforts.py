ca322408868e3a10df2e8e146e58c7283cd7a40b
robgriffin247 <robgriffinwork@gmail.com>
2024-05-16 14:40:29 +0200

    added nesting of data in get segment efforts

--- app/strava_api/get_segment_efforts.py
+++ app/strava_api/get_segment_efforts.py
@@ -30,28 +30,35 @@ def get_segment_efforts(id=17267489, window=90, since="1970-01-01"):
         content = content + response.json()
 
     output = {
-        'athlete_name': f"{athlete_details['firstname']} {athlete_details['lastname']}",
-        'athlete_id': athlete_details['id'],
-        'segment_name': content[0]["name"],
-        'segment_id': content[0]["segment"]["id"],
-        'effort_count_all_time': len(content),
-        'efforts': pd.DataFrame({
+        'athlete': {
+            'name': f"{athlete_details['firstname']} {athlete_details['lastname']}",
+            'id': athlete_details['id']   
+        },
+        'segment': {
+            'segment_name': content[0]["name"],
+            'segment_id': content[0]["segment"]["id"]
+        },
+        'efforts': {
+            'all_time': len(content)
+        },
+        'z_data': pd.DataFrame({
             'effort_id': [effort["id"] for effort in content],
             'effort_datetime': [datetime.strptime(effort["start_date"], "%Y-%m-%dT%H:%M:%SZ") for effort in content],
             'activity_id': [effort["activity"]["id"] for effort in content],
             'days_since': [(datetime.now() - datetime.strptime(effort["start_date"], "%Y-%m-%dT%H:%M:%SZ")).days for effort in content],
             'seconds': [effort["elapsed_time"] for effort in content],
             'time': [str(timedelta(seconds=effort["elapsed_time"]))  for effort in content]
-        })
+        }),
+        'bests': {}
     }
 
-    output["best_all_time"] = output["efforts"]["time"][0]
-    output["best_window"] = output["efforts"].loc[output["efforts"]["days_since"]<=window]["time"].iloc[0]
-    output["best_since"] = output["efforts"].loc[output["efforts"]["effort_datetime"]>=datetime.strptime(f"{since}T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ")]["time"].iloc[0]
+    output["bests"]["all_time"] = output["z_data"]["time"][0]
+    output["bests"]["window"] = output["z_data"].loc[output["z_data"]["days_since"]<=window]["time"].iloc[0]
+    output["bests"]["since"] = output["z_data"].loc[output["z_data"]["effort_datetime"]>=datetime.strptime(f"{since}T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ")]["time"].iloc[0]
     
-    output["effort_count_window"] = len(output["efforts"].loc[output["efforts"]["days_since"]<=window])
-    output["effort_count_since"] = len(output["efforts"].loc[output["efforts"]["effort_datetime"]>=datetime.strptime(f"{since}T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ")])
+    output["efforts"]["window"] = len(output["z_data"].loc[output["z_data"]["days_since"]<=window])
+    output["efforts"]["since"] = len(output["z_data"].loc[output["z_data"]["effort_datetime"]>=datetime.strptime(f"{since}T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ")])
     
-    output["most_efforts_in_one_activity"] = output["efforts"].groupby("activity_id").size().max()
+    #output["most_efforts_in_one_activity"] = output["z_data"].groupby("activity_id").size().max()
 
     return output
