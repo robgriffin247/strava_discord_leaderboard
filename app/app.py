ba2c5bebe7194015f460a5c0cefbfa046701d57d
robgriffin247 <robgriffinwork@gmail.com>
2024-05-16 09:34:26 +0200

    removed src folder

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import pprint as pp
 
-from strava_api.src.get_athlete_details import get_athlete_details
-from strava_api.src.get_segment_efforts import get_segment_efforts
+from strava_api.get_athlete_details import get_athlete_details
+from strava_api.get_segment_efforts import get_segment_efforts
 
 pp.pprint(get_athlete_details(), indent=2)
 pp.pprint(get_segment_efforts(), indent=2)
